bb696993fd2f8b660b5e33ad35a9f024110c6ff7
Andrea Jao <ndrjao@gmail.com>
2024-05-14 17:37:09 -0700

    added test

--- markov.test.js
+++ markov.test.js
@@ -1,11 +1,11 @@
-import { describe, test, expect, beforeEach} from "vitest";
+import { describe, test, expect, beforeEach } from "vitest";
 import { MarkovMachine } from "./markov";
 
-describe("Testing getChains", function(){
-  test("testing no duplicate words", function(){
+describe("Testing getChains", function () {
+  test("testing no duplicate words", function () {
     const markovTest = new MarkovMachine("The cat in the hat.");
     const markovChains = markovTest.chains;
-    
+
     expect(markovChains).toEqual({
       "The": ["cat"],
       "cat": ["in"],
@@ -14,23 +14,36 @@ describe("Testing getChains", function(){
       "hat.": [null],
     });
   });
-  
-  test("testing duplicate words",function(){
+
+  test("testing duplicate words", function () {
     const markovTest = new MarkovMachine(
       "The cat is in the hat. The cat is the cat. The hat is a cat."
     );
     const markovChains = markovTest.chains;
-    
+
     expect(markovChains).toEqual({
-      "The":  ["cat", "cat", "hat"],
-      "cat":  ["is", "is"],
-      "is":   ["in", "the", "a"],
-      "in":   ["the"],
-      "the":  ["hat.", "cat."],
+      "The": ["cat", "cat", "hat"],
+      "cat": ["is", "is"],
+      "is": ["in", "the", "a"],
+      "in": ["the"],
+      "the": ["hat.", "cat."],
       "hat.": ["The"],
       "cat.": ["The", null],
-      "hat":  ["is"],
-      "a":    ["cat."],
+      "hat": ["is"],
+      "a": ["cat."],
     });
   });
+});
+
+
+describe("Testing getText", function () {
+  test("testing getting string with chains object", function () {
+    const markovTest = new MarkovMachine("The cat in the hat.");
+    const markovGetText = markovTest.getText();
+
+    expect(markovGetText).toEqual(
+      "The cat in the hat."
+    );
+  });
+
 });
